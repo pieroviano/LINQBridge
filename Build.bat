971c8a172c4266e08f8f08164987692a5ecf4a59
Piero Viano <piero.viano@gmail.com>
2023-04-09 06:05:35 +0300

    ''

--- Build.bat
+++ Build.bat
@@ -3,7 +3,6 @@ del ..\WhenTheVersion\Packages\Net30.LinqBridge.*
 MSBuild.exe Net30.LinqBridge.sln -m /property:Configuration=%Configuration% 
 cd Packages
 FOR %%i IN (Net30.LinqBridge.*.nupkg) DO dotnet nuget push %%i --source NuGet --api-key %ApiKey% -t 1000
-FOR %%i IN (Net30.LinqBridge.*.snupkg) DO dotnet nuget push %%i --source NuGet --api-key %ApiKey% -t 1000
 cd ..
 git add -A
 git commit -a --allow-empty-message -m ''
