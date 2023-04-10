2c46d0ef5f79a3bbd8f79168a73f9f3c129e5a41
Piero Viano <piero.viano@gmail.com>
2023-04-10 03:55:11 +0300

    ''

--- Build.bat
+++ Build.bat
@@ -1,9 +1,11 @@
 MSBuild.exe Net30.LinqBridge.sln -t:restore -p:RestorePackagesConfig=true
 del ..\WhenTheVersion\Packages\Net30.LinqBridge.*
 MSBuild.exe Net30.LinqBridge.sln -m /property:Configuration=%Configuration% 
-cd Packages
-FOR %%i IN (Net30.LinqBridge.*.nupkg) DO dotnet nuget push %%i --source NuGet --api-key %ApiKey% -t 1000
-cd ..
+IF DEFINED Package (
+	cd Packages
+	FOR %%i IN (Net30.LinqBridge.*.nupkg) DO dotnet nuget push %%i --source NuGet --api-key %ApiKey% -t 1000
+	cd ..
+)
 git add -A
 git commit -a --allow-empty-message -m ''
 git push
